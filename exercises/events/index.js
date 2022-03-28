392e94e9a94b8e1c8ad77521a4cb4259b5efa129
nigmasilmi <sn.silmi@gmail.com>
2022-03-28 12:11:21 -0300

    Events and sorting

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,35 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
+  constructor() {
+    this.events = {};
+  }
   // Register an event handler
-  on(eventName, callback) {}
+  on(eventName, callback) {
+    if (!this.events[eventName]) {
+      this.events[eventName] = [callback];
+    } else {
+      this.events[eventName].unshift(callback);
+    }
+  }
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    for (let cb of this.events[eventName]) {
+      cb();
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
-  off(eventName) {}
+  off(eventName) {
+    if (this.events[eventName]) {
+      this.events[eventName] = [];
+    } else {
+      return;
+    }
+  }
 }
 
 module.exports = Events;
